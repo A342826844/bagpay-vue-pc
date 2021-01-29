2ca8352a6afa02f3787f1f921a90552aee679c6e
aixiaolong <342826844@qq.com>
2021-01-29 17:17:21 +0800

    init

---
+++ js/lang.js
@@ -0,0 +1,20 @@
+var langMessage = {
+    'zh-CN': {
+        title1: '多币种钱包',
+        tip1:'支持多种主流币种 跨链交易 方便高效',
+        title2:'多重保障 更加安全',
+        tip2:'私钥多重保护 账户安全无需担心',
+        title3:'欢迎来到Bag Pay',
+        tip3:'安全放心 方便快捷',
+        download: '下载',
+    },
+    'en-us': {
+        title1: 'Multi currency wallet',
+        tip1: 'It supports cross chain transactions in multiple mainstream currencies, which is convenient and efficient',
+        title2: 'Multiple guarantees',
+        tip2: 'Private key multiple protection, no need to worry about account security',
+        title3: 'Welcome to Bag Pay',
+        tip3: 'Safe, safe, convenient and fast',
+        download: 'Download',
+    },
+};
